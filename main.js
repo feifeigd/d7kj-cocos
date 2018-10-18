f579213fec27fcdf4746fb880a63e8f7cb79abd8
luofei <13632323692@139.com>
2018-10-18 17:21:45 +0800

    生成标准资源目录

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const Fs = require('fire-fs');
+const Path = require('fire-path');
+
 module.exports = {
   load () {
     // execute when package loaded
@@ -19,6 +22,18 @@ module.exports = {
       Editor.log('Hello World!');
       // send ipc message to panel
       Editor.Ipc.sendToPanel('d7kj-cocos', 'd7kj-cocos:hello');
+    },
+    // 生成标准资源目录
+    'gen_std_dirs' () {
+      let create = (sub_dir)=>{
+        let path = Path.join(Editor.projectPath, 'assets', sub_dir);
+        Fs.mkdirsSync(path);
+        Editor.assetdb.refresh('db://assets/' + sub_dir, ()=>{
+          Editor.success('create dir ' + sub_dir + ' success!');
+        });
+      };
+      let sub_dirs = ['res', 'scenes', 'scripts', ];
+      sub_dirs.map((dir)=>{create(dir);});
     },
     'clicked' () {
       Editor.log('Button clicked!');
